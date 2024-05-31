4b463f3d1e2c6b8e2afb61a7d4184f28540159f8
Abdur Rahim <rahim11500000@gmail.com>
2024-05-31 22:04:31 +0600

    Setup Dream Dwellings server

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express()
+
+// middlewars
+app.use(express.json());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173"
+        ],
+        credentials: true,
+    })
+);
+
+// APIs
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster2.n2vc9uo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // app.get("/")
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Dream Dwellings is running")
+})
+app.listen(port, () => {
+    console.log(`Drem Dwelling is running at: http://localhost:${port}`)
+})
