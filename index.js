8c4f0236b7ca9762a7e0bc3eca29c9911f12b613
Abdur Rahim <rahim11500000@gmail.com>
2024-06-02 00:24:50 +0600

    make properties property details add to wishlist and reviews APIs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -36,6 +36,8 @@ async function run() {
         const dreamDwellings = client.db("dreamDwellings");
         const advertisementsCollection = dreamDwellings.collection("advertisements");
         const reviewsCollection = dreamDwellings.collection("reviews");
+        const propertiesCollection = dreamDwellings.collection("properties");
+        const wishlistCollection = dreamDwellings.collection("wishlist");
 
 
         // APIs
@@ -43,8 +45,29 @@ async function run() {
             const cursor = await advertisementsCollection.find().toArray();
             res.send(cursor)
         })
-        app.get("/reviews", async (req, res) => {
+        app.get("/latest-reviews", async (req, res) => {
             const cursor = await reviewsCollection.find().toArray();
+            res.send(cursor)
+        })
+        app.get("/review", async (req, res) => {
+            const propertyId = req.query.propertyId
+            const query = { property_id: propertyId }
+            const cursor = await reviewsCollection.find(query).toArray();
+            res.send(cursor)
+        })
+        app.get("/properties", async (req, res) => {
+            const cursor = await propertiesCollection.find().toArray();
+            res.send(cursor)
+        })
+        app.get("/property-details", async (req, res) => {
+            const id = req.query.id
+            const query = { _id: new ObjectId(id) }
+            const cursor = await propertiesCollection.findOne(query);
+            res.send(cursor)
+        })
+        app.post("/add-to-wishlist", async (req, res) => {
+            const body = req.body
+            const cursor = await wishlistCollection.insertOne(body)
             res.send(cursor)
         })
 
